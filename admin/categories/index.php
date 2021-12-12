d27b70ce566115859aa2cc2cae46c2f61456c1be
luong ngoc <ngocthien250800@gmail.com>
2021-12-12 23:19:06 +0700

    create showAdmin_categoriesTable

---
+++ admin/categories/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <!-- Required meta tags -->
+     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title></title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="../assets/vendors/iconfonts/mdi/css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="../assets/vendors/iconfonts/ionicons/dist/css/ionicons.css">
+    <link rel="stylesheet" href="../assets/vendors/iconfonts/flag-icon-css/css/flag-icon.min.css">
+    <link rel="stylesheet" href="../assets/vendors/css/vendor.bundle.base.css">
+    <link rel="stylesheet" href="../assets/vendors/css/vendor.bundle.addons.css">
+    <!-- endinject -->
+    <!-- plugin css for this page -->
+    <link rel="stylesheet" href="../assets/vendors/icheck/skins/all.css">
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <link rel="stylesheet" href="../assets/css/shared/style.css">
+    <!-- endinject -->
+    <!-- Layout styles -->
+    <link rel="stylesheet" href="../assets/css/demo_1/style.css">
+</head>
+<body>
+
+<?php
+include '../../dbconnect.php';
+$stmt = $pdo->prepare('SELECT * from categories');
+$stmt->setFetchMode(PDO::FETCH_ASSOC);
+$stmt->execute();
+?>
+
+<div class="card-body">
+ <h4 class="card-title">DANH SÁCH LOẠI SẢN PHẨM</h4>
+ <table class="table table-bordered">
+    <thead>
+      <tr>
+        <th>Số thứ tự</th>
+        <th>Tên Loại sản phẩm</th>
+        
+        <th>Chức năng</th>
+      </tr>
+    </thead>
+    <tbody>
+        <?php
+        $i = 1;  
+        while($row = $stmt->fetch()){ 
+            ?>
+      <tr>
+        <td><?php echo $i++ ?></td>
+        <td><?php echo $row['name'] ?></td> 
+        <td><a class="btn btn-primary" href="edit.php?id=<?php echo $row['id']?>">Sửa</a></td> 
+        <td><a class="btn btn-primary" href="index.php?id=<?php echo $row['id']?>">Xóa</a></td>     
+      </tr>    
+      <?php }?>
+    </tbody>
+  </table>
+  <a class="btn btn-primary" style = "margin-top:10px;" href="create.php">Thêm</a>
+</div>
+  
+</body>
+</html>
